1ad2169ea8dc169f5ce5f6175a274fb2efcf71ef
kentxxq <kentxxq@gmail.com>
2026-01-19 14:10:46 +0800

    调整打包的输出目录

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  outDir: 'dist', // 修改输出目录名称，默认是 .output
   manifest: {
     name: '__MSG_extension_name__',
     description: '__MSG_extension_description__',
